ff3b4333420a5dbc7812d3513492eb5bc4ddd24e
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-07-19 18:21:53 +0700

    Add endpoint Post /api/tasks

--- main.go
+++ main.go
@@ -15,6 +15,14 @@ type Task struct {
 	Title string
 }
 
+type CreateTaskInput struct {
+	Title string `json:"title"`
+}
+
+type CreateTaskResponse struct {
+	Message string `json:"message"`
+}
+
 var tasks []Task
 
 func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,11 +36,30 @@ func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func createTasksHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json");
+	w.WriteHeader(http.StatusCreated)
+
+	var taskInput CreateTaskInput
+	if err := json.NewDecoder(r.Body).Decode(&taskInput); err != nil {
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	tasks = append(tasks, Task(taskInput))
+
+	if err := json.NewEncoder(w).Encode(CreateTaskResponse{Message: "Success create task item"}); err != nil {
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+}
+
 func main() {
 	// add initial tasks
 	tasks = append(tasks, Task{Title: "Implement Get tasks"}, Task{Title: "Implement Create a task"})
 
 	http.HandleFunc("GET /api/tasks", GetTasksHandler)
+	http.HandleFunc("POST /api/tasks", createTasksHandler)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
